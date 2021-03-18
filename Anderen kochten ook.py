022222f6bb0cc33bacae3243b4545ad29d2f3ccd
Expensure <jaspervanloon@outlook.com>
2021-03-18 16:46:57 +0100

    Poging gedaan om anderen kochten ook te doen. maar loop vast

--- Anderen kochten ook.py
+++ Anderen kochten ook.py
@@ -1,4 +1,4 @@
-import psycopg2, csv, ast
+import psycopg2, csv, ast, random
 
 connection = psycopg2.connect(user="postgres",
                               password="postgres",
@@ -72,11 +72,52 @@ def get_products_viewed():
     query = '''SELECT * FROM profiles_previously_viewed '''
     cursor.execute(query)
     rows = cursor.fetchall()
+    recommendations_per_user = []
     for row in rows:
-        
-        print(row)
+        users = []
+        for bow in rows:
+            if len(row) == 2 and len(bow) == 2:
+                if row[1] != '' and bow[1] != '':
+                    if row[0] != bow[0]:
+                        if row[1] == bow[1]:
+                            if bow[0] not in users:
+                                users.append(bow[0])
+        recommendations = []
+        try:
+            ranselect_user = random.sample(users, 5)
+        except:
+            cursor.execute("SELECT products_ids FROM profiles_previously_viewed")
+            rows = cursor.fetchall()
+            recommendations.append(random.sample(rows, 5))
+            recommendations_per_user.append([row[0], random.sample(rows, 5)])
+            break
+
+        for i in ranselect_user:
+            cursor.execute("SELECT products_ids FROM profiles_previously_viewed WHERE profiles_id = %(value)s """,
+                           {"value": i})
+            cows = cursor.fetchall()
+            recommendations.append(list(random.choice(cows)))
+            print(recommendations)
+        recommendations_per_user.append([row[0], recommendations])
+        print(recommendations_per_user)
+    return recommendations_per_user
 
 
-get_products_viewed()
+def make_csv(recommendations):
+    with open('anderen_kochten_ook.csv', 'w') as writable:
+        names = ['profiles_id', 'products_id1', 'products_id2', 'products_id3', 'products_id4', 'products_id5']
+        writer = csv.DictWriter(writable, fieldnames=names)
+        writer.writeheader()
+        for item in recommendations:
+            writer.writerow(
+                {'profiles_id': item,
+                 'products_id1': recommendations[item][0],
+                 'products_id2': recommendations[item][1],
+                 'products_id3': recommendations[item][2],
+                 'products_id4': recommendations[item][3],
+                 'products_id5': recommendations[item][4]}
+            )
+
+make_csv(get_products_viewed())
 
 connection.close()
